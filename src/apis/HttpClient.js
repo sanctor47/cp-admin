846c087f05291c3497fe650a73479bbff6630506
aser <aser.e.nabil@gmail.com>
2022-10-16 02:42:47 +0200

    Build

--- src/apis/HttpClient.js
+++ src/apis/HttpClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const HttpClient = axios.create({
-  // baseURL: "http://18.185.66.214:5000/api/v1",
-  baseURL: "http://127.0.0.1:5000/api/v1",
+  baseURL: "http://18.185.66.214:5000/api/v1",
+  // baseURL: "http://127.0.0.1:5000/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
